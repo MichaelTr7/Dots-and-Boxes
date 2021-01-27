d66e5b0015fa19d1d599844bd9ace2f403d4e941
Michael <mailmichaeltr@gmail.com>
2021-01-27 02:52:46 -0500

    Major How To Play Page Overhaul

--- JavaScript/About_Page.js
+++ JavaScript/About_Page.js
@@ -4,4 +4,7 @@ window.onload = function(){
   document.getElementsByClassName('Navigation_Buttons')[0].addEventListener("click",Direct_To_Back_To_Page);
   document.getElementsByClassName('Navigation_Buttons')[1].addEventListener("click",Direct_To_Back_To_Page);
   document.getElementById('Backdrop').addEventListener("click",Close_Panels);
+  document.getElementById('Gameplay_Tutorial_Grid').addEventListener("click",Close_Panels);
+  document.getElementById('Game_Banner').addEventListener("click",Close_Panels);
+
 }
